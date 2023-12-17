2fd5757217450381db2dd7900d7dfca5deec843d
keychera <kyogatama@gmail.com>
2023-12-17 10:18:26 +0700

    game 2 runestone

--- rune/Runestone.gd
+++ rune/Runestone.gd
@@ -1,23 +1,25 @@
-@tool
-extends Sprite2D
+extends TextureButton
 class_name Runestone
 
 @onready var mat = (material as ShaderMaterial)
 
+@export var debug : Vector2
+
 func _process(delta):
 	mat.set_shader_parameter("time", mat.get_shader_parameter("time") + delta)
+	debug = Vector2(get_local_mouse_position().x - 30., get_local_mouse_position().y - 30.)
 	
 
 func _on_mouse_entered():
-	print("enter")
 	mat.set_shader_parameter("pos1", mat.get_shader_parameter("pos2"))
 	mat.set_shader_parameter("pos2", Vector2(0, 0))
 	mat.set_shader_parameter("time", 0.)
 
 
 func _on_mouse_exited():
-	print("exit")
-	var normalized_mouse = get_local_mouse_position()/60;
+	var original = get_local_mouse_position()
+	var offset_mouse = Vector2(original.x - 30., original.y - 30.)
+	var normalized_mouse = offset_mouse/60;
 	mat.set_shader_parameter("pos1", mat.get_shader_parameter("pos2"))
 	if (abs(normalized_mouse.x) < 1. && abs(normalized_mouse.y) < 1.):
 		mat.set_shader_parameter("pos2", sign(round(normalized_mouse)) * 0.18)
